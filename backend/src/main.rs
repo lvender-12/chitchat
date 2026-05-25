a7d4aff12360c380d7986e1b530c14eab12fbd5b
lvander-12 <muhamaddamar1812@gmail.com>
2026-05-25 15:13:33 +0700

    depedencies

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,13 @@
+use rand::RngExt;
+
 fn main() {
     println!("Hello, world!");
+    let uuid = generate_uuid();
+    println!("{}", uuid)
+}
+
+fn generate_uuid() -> u64 {
+    let mut rng = rand::rng();
+
+    rng.random_range(100_000_000..=999_999_999)
 }
